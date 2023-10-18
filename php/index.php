c3ad2e78afd5c9a8721fef20eaf99ad6f4a9ae7e
yonev <yv4.student.waikato@gmail.com>
2023-10-19 12:48:43 +1300

    moved over changes

--- php/index.php
+++ php/index.php
@@ -1,65 +1,73 @@
+<?php
+// Report all errors
+ini_set("error_reporting",E_ALL);
+?> 
+
+<?php
+// Read the notes.xml data file if it exists
+if (file_exists("catalog.xml")) 
+    $catalog=simplexml_load_file('catalog.xml');
+else 
+    exit('Failed to open catalog.xml.');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plant Catalog</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <h1>Plant Catalog</h1>
-
     <form action="" method="post">
         <label for="sort">Sort by:</label>
         <select name="sort" id="sort">
             <option value="name">Name</option>
             <option value="age">Age</option>
         </select>
-        <input type="submit" value="Sort">
+        <input type="submit" value="sort">
+    </form>
+
+    <form action="" method="post">
+    <input type="submit" name="showAll" value="Show All">
     </form>
 
     <form action="" method="post">
         <label for="search">Search by title:</label>
         <input type="text" name="search" id="search" required>
         <input type="submit" value="Search">
-    </form>
+        <?php
+        // // Read the plant catalog
+        // $catalog = simplexml_load_file("catalog.xml");
+        echo "<ul>";
+        
+        echo "<ul>";
+        // Print out all the titles in the catalog
+        // echo "Titles in the catalogue:\n";
+        // echo "<ul>";
+        // $results=$catalog->xpath("//name");
+        // foreach ($results as $result){
+        //    // echo "  $result\n\r";
+        //    echo "<li>$result</li>";
+        // }
+        // echo "</ul>";
 
-    <?php
-        // Load and parse the XML file
-        $xml = simplexml_load_file('catalog.xml');
-
-        // Function to sort plants by name
-        function sortByName($a, $b) {
-            return strcmp($a->name, $b->name);
-        }
-
-        // Function to sort plants by age
-        function sortByAge($a, $b) {
-            return intval($a->age) - intval($b->age);
-        }
-
-        // Determine the sorting method
-        if(isset($_POST['sort'])) {
-            $sortMethod = $_POST['sort'];
-
-            switch($sortMethod) {
-                case 'name':
-                    usort($xml->plant, 'sortByName');
-                    break;
-                case 'age':
-                    usort($xml->plant, 'sortByAge');
-                    break;
-                default:
-                    break;
+            // Check if the "Show All" button was clicked
+            if(isset($_POST['showAll'])) {
+                // Reload the page to display all plants
+                header("Location: ".$_SERVER['PHP_SELF']);
+                exit();
             }
-        }
 
         // Search functionality
-        if(isset($_POST['search'])) {
+        if(isset($_POST['search'])) {   
             $searchTerm = $_POST['search'];
 
             // Iterate through each plant and display matching results
-            foreach ($xml->plant as $plant) {
+            //echo "Read in a catalog with ".$catalog->count()." Plants.\n";
+            foreach ($catalog->plant as $plant) {
                 if (stripos($plant->name, $searchTerm) !== false) {
                     echo "<div class='plant'>";
                     echo "<h2>{$plant->name}</h2>";
@@ -72,7 +80,8 @@
             }
         } else {
             // If no search term is provided, display all plants
-            foreach ($xml->plant as $plant) {
+            foreach ($catalog->plant as $plant) {
+                
                 echo "<div class='plant'>";
                 echo "<h2>{$plant->name}</h2>";
                 echo "<p>Type: {$plant->type}</p>";
@@ -81,8 +90,35 @@
                 echo "<p>Watering: {$plant->watering}</p>";
                 echo "</div>";
             }
-        }
-    ?>
+        }    
 
+        //Function to sort plants by name
+        function sortByName($a, $b) {
+            return strcmp($a->name, $b->name);
+        }
+
+        //Function to sort plants by age
+        function sortByAge($a, $b) {
+            return intval($a->age) - intval($b->age);
+        }
+
+        // Determine the sorting method
+        if(isset($_POST['sort'])) {
+            $sortMethod = $_POST['sort'];
+
+            switch($sortMethod) {
+                case 'name':
+                    usort($catalog->plant, 'sortByName');
+                    break;
+                case 'age':
+                    usort($catalog->plant, 'sortByAge');
+                    break;
+                default:
+                    break;
+            }
+        }
+        ?>
+    </form>
 </body>
 </html>
+
