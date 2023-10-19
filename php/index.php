ef565558b40f59e853aae7d757494404c27df846
yonev <yv4.student.waikato@gmail.com>
2023-10-19 20:21:14 +1300

    all works not sort

--- php/index.php
+++ php/index.php
@@ -11,6 +11,7 @@ else
     exit('Failed to open catalog.xml.');
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,6 +21,7 @@ else
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+<div class="plant">
     <h1>Plant Catalog</h1>
     <form action="" method="post">
         <label for="sort">Sort by:</label>
@@ -28,20 +30,56 @@ else
             <option value="age">Age</option>
         </select>
         <input type="submit" value="sort">
+        <input type="submit" name="showAll" value="Show All">
     </form>
-
-    <form action="" method="post">
-    <input type="submit" name="showAll" value="Show All">
-    </form>
-
     <form action="" method="post">
         <label for="search">Search by title:</label>
         <input type="text" name="search" id="search" required>
         <input type="submit" value="Search">
-        <?php
-        echo "<ul>";
         
-        echo "<ul>";
+    </form>
+    </div>
+    <?php
+// Convert SimpleXMLElement to an array
+// $plants = iterator_to_array($catalog->plant);
+
+//  // Function to sort plants by name
+//  function sortByName($a, $b) {
+//     return strcmp($a->name, $b->name);
+// }
+
+// // Function to sort plants by age
+// function sortByAge($a, $b) {
+//     return intval($a->age) - intval($b->age);
+// }
+
+// Determine the sorting method
+// if(isset($_POST['sort'])) {
+//     $sortMethod = $_POST['sort'];
+
+//     switch($sortMethod) {
+//         case 'name':
+//             usort($plants, 'sortByName');
+//             break;
+//         case 'age':
+//             usort($plants, 'sortByAge');
+//             break;
+//         default:
+//             break;
+//     }
+// }
+// Iterate through each plant and display the information
+// foreach ($plants as $plant) {
+//     echo "<div class='plant'>";
+//     echo "<h2>{$plant->name}</h2>";
+//     echo "<p>Type: {$plant->type}</p>";
+//     echo "<p>Light: {$plant->light}</p>";
+//     echo "<p>Age: {$plant->age}</p>";
+//     echo "<p>Watering: {$plant->watering}</p>";
+//     echo "<img src='{$plant->image}' alt='{$plant->name}'>";
+//     echo "</div>";
+// }
+
             // Check if the "Show All" button was clicked
             if(isset($_POST['showAll'])) {
                 // Reload the page to display all plants
@@ -56,32 +94,36 @@ else
             // Iterate through each plant and display matching results
             //echo "Read in a catalog with ".$catalog->count()." Plants.\n";
             foreach ($catalog->plant as $plant) {
-                if (stripos($plant->name, $searchTerm) !== false){
-                    echo "<div class='plant'>";
-                    echo "<h2><a href='display.php?id={$plant['id']}'>{$plant->name}</a></h2>";
-                    echo "<p>Type: {$plant->type}</p>";
-                    echo "<p>Light: {$plant->light}</p>";
-                    echo "<p>Age: {$plant->age}</p>";
-                    echo "<p>Watering: {$plant->watering}</p>";
-                    echo "<img src='{$plant->image}' alt='{$plant->name}'>";
-                    echo "</div>";
-                }
-            }
-        } else {
-            // If no search term is provided, display all plants
-            foreach ($catalog->plant as $plant) {
+                if (stripos($plant->name, $searchTerm) !== false) {
+                echo "<a href='display.php?id={$plant['id']}' class='plant-card'>";
                 echo "<div class='plant'>";
-                echo "<h2><a href='display.php?id={$plant['id']}'>{$plant->name}</a></h2>";
+                echo "<h2>{$plant->name}</h2>";
                 echo "<p>Type: {$plant->type}</p>";
                 echo "<p>Light: {$plant->light}</p>";
                 echo "<p>Age: {$plant->age}</p>";
                 echo "<p>Watering: {$plant->watering}</p>";
                 echo "<img src='{$plant->image}' alt='{$plant->name}'>";
                 echo "</div>";
+                echo "</a>";
+                }
+                
+            }
+        } else {
+            // If no search term is provided, display all plants
+            foreach ($catalog->plant as $plant) {
+                echo "<a href='display.php?id={$plant['id']}' class='plant-card'>";
+                echo "<div class='plant'>";
+                echo "<h2>{$plant->name}</h2>";
+                echo "<p>Type: {$plant->type}</p>";
+                echo "<p>Light: {$plant->light}</p>";
+                echo "<p>Age: {$plant->age}</p>";
+                echo "<p>Watering: {$plant->watering}</p>";
+                echo "<img src='{$plant->image}' alt='{$plant->name}'>";
+                echo "</div>";
+                echo "</a>";
             }
         }
          ?>
-    </form>
 </body>
 </html>
 
